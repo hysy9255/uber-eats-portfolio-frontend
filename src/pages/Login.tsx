8b1fa8b071738d1d73ffedce8b21afc18bc74caf
HyungJoonYoon <hysy9255@gmail.com>
2026-04-10 19:01:50 +0900

    pipeline test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -63,7 +63,7 @@ export default function Login() {
                         p-5 flex flex-col `}
         >
           <h1 className="text-xl font-extrabold tracking-tight">
-            Welcome back 10
+            Welcome back
           </h1>
           <p className="mt-2 text-sm text-slate-600">Sign in to continue</p>
 
